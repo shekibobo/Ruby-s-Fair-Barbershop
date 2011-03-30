9e6ae342cf4a7a985241c256e0df1cc0c7b4b76d
Joshua Kovach <kovach.jc@gmail.com>
2011-03-29 21:03:54 -0400

    fixed error in semaphore exclusion block

--- semaphore.rb
+++ semaphore.rb
@@ -7,32 +7,29 @@ class Semaphore
   end
 
   def wait
-    Thread.critical = true
-    # decrement and add to the waiting list
-    @counter -= 1
-    if @counter < 0
-      @waitingList.push(Thread.current)
-      Thread.stop
+    Thread.exclusive do
+      # decrement and add to the waiting list
+      @counter -= 1
+      if @counter < 0
+        @waitingList.push(Thread.current)
+        Thread.stop
+      end
+      self # return itself
     end
-    self # return itself
-  # not sure what this does yet
-  ensure
-    Thread.critical = false
   end
 
   def signal
-    Thread.critical = true
-    begin
-      @counter += 1
-      if @counter <= 0
-        thread = @waitingList.shift # FIFO => grab first added
-        thread.wakeup if thread # if the shift produced a thread, wake it up
+    Thread.exclusive do
+      begin
+        @counter += 1
+        if @counter <= 0
+          thread = @waitingList.shift # FIFO => grab first added
+          thread.wakeup if thread # if the shift produced a thread, wake it up
+        end
+      rescue ThreadError
+        retry
       end
-    rescue ThreadError
-      retry
+      self # return itself
     end
-    self # return itself
-  ensure
-    Thread.critical = false
   end
 end
